8b66ce4bab0434839b4ae1142020418adc5bba60
kimdonwoo <ehstjstod@gmail.com>
2023-09-26 12:13:35 +0900

    Feat: 프로젝트 초기 설정 완료
    프로젝트 생성 후 기본 설정과 환경 설정까지 완료하였습니다.

---
+++ wekiki/src/main/java/com/kakao/techcampus/wekiki/WekikiApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.techcampus.wekiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WekikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WekikiApplication.class, args);
+	}
+
+}
---
+++ wekiki/src/test/java/com/kakao/techcampus/wekiki/WekikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.techcampus.wekiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WekikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
