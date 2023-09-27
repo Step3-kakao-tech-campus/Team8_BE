a1feb6fc9dadcb4872d0ef0e5b7b77e717b6d52d
INUK-ai <arheon2469@gmail.com>
2023-09-28 07:18:16 +0900

    Feat: 그룹 관련 객체 생성
    - SINGLE_TABLE 상속을 사용한 Group 관련 객체 생성
        - Group
        - OfficialGroup
        - UnOfficialClosedGroup
        - UnOfficialOpenedGroup

---
+++ wekiki/src/main/java/com/kakao/techcampus/wekiki/group/unOfficialGroup/closedGroup/UnOfficialOpenedGroup.java
@@ -0,0 +1,30 @@
+package com.kakao.techcampus.wekiki.group.unofficialGroup.openedGroup;
+
+import com.kakao.techcampus.wekiki.group.Group;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue("un_official_opened_group")
+public class UnOfficialOpenedGroup extends Group {
+
+    private String introduction;
+    private String entranceHint;
+    private String entrancePassword;
+
+    @Builder(builderMethodName = "unOfficialOpenedGroupBuilder")
+    public UnOfficialOpenedGroup(Long id, String groupName, String groupProfileImage, int memberCount, LocalDateTime created_at, String introduction, String entranceHint, String entrancePassword) {
+        super(id, groupName, groupProfileImage, memberCount, created_at);
+        this.introduction = introduction;
+        this.entranceHint = entranceHint;
+        this.entrancePassword = entrancePassword;
+    }
+}
