a5567561c63f1309a1ed24efda420fe6cb46ae7f
INUK-ai <arheon2469@gmail.com>
2023-09-28 07:35:32 +0900

    Feat: 댓글 객체 생성
    - Comment 객체 생성
    - Post 별 Comment라고 생각해 Post 패키지 내부에 Comment 패키지 생성

---
+++ wekiki/src/main/java/com/kakao/techcampus/wekiki/page/post/comment/Comment.java
@@ -0,0 +1,37 @@
+package com.kakao.techcampus.wekiki.page.post.comment;
+
+import com.kakao.techcampus.wekiki.member.Member;
+import com.kakao.techcampus.wekiki.page.post.Post;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comment_tb")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Member member;
+    @ManyToOne
+    private Post post;
+    private String content;
+    private LocalDateTime created_at;
+
+    @Builder
+    public Comment(Long id, Member member, Post post, String content, LocalDateTime created_at) {
+        this.id = id;
+        this.member = member;
+        this.post = post;
+        this.content = content;
+        this.created_at = created_at;
+    }
+}
