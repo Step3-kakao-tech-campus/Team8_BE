590b98cbaa50e822101bc9ae2ee0f946eba12274
INUK-ai <arheon2469@gmail.com>
2023-09-28 07:30:06 +0900

    Feat: 페이지 객체 생성
    - Page 객체 생성
    - 페이지 좋아요 수, 싫어요 수, 조회 수를 따로 저장

---
+++ wekiki/src/main/java/com/kakao/techcampus/wekiki/page/Page.java
@@ -0,0 +1,39 @@
+package com.kakao.techcampus.wekiki.page;
+
+import com.kakao.techcampus.wekiki.group.Group;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "page_tb")
+public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Group group;
+    private String title;
+    private int goodCount;
+    private int badCount;
+    private int viewCount;
+    private LocalDateTime created_at;
+
+    @Builder
+    public Page(Long id, Group group, String title, int goodCount, int badCount, int viewCount, LocalDateTime created_at) {
+        this.id = id;
+        this.group = group;
+        this.title = title;
+        this.goodCount = goodCount;
+        this.badCount = badCount;
+        this.viewCount = viewCount;
+        this.created_at = created_at;
+    }
+}
