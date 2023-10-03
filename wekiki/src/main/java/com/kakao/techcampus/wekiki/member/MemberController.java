adb89b721c3380e614fe57410e2d6f16ef8cc920
Jun2-Lee <ljh19980823@gmail.com>
2023-10-03 09:16:12 +0900

    Fix : @RequestBody 추가 - DTO를 Body 형식으로 받아오게끔 수정

--- wekiki/src/main/java/com/kakao/techcampus/wekiki/member/MemberController.java
+++ wekiki/src/main/java/com/kakao/techcampus/wekiki/member/MemberController.java
@@ -3,6 +3,7 @@ package com.kakao.techcampus.wekiki.member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signUp(MemberRequest.signUpDTO signUpDTO) {
+    public ResponseEntity<?> signUp(@RequestBody MemberRequest.signUpDTO signUpDTO) {
         memberService.signUp(signUpDTO);
         return ResponseEntity.ok(true);
     }
