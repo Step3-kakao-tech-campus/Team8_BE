96c515b3913e286f0d4247a45d71ead2ea4ddfd0
Jun2-Lee <ljh19980823@gmail.com>
2023-10-03 09:07:53 +0900

    Feat : Security Config 파일 수정 - Security Config 파일을 최신 버전으로 수정하였습니다(URL 모두 열어놓은 것은 동일합니다)

--- wekiki/src/main/java/com/kakao/techcampus/wekiki/_core/config/SecurityConfig.java
+++ wekiki/src/main/java/com/kakao/techcampus/wekiki/_core/config/SecurityConfig.java
@@ -1,2 +1,43 @@
-package com.kakao.techcampus.wekiki._core.config;public class SecurityConfig {
+package com.kakao.techcampus.wekiki._core.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests.anyRequest().permitAll());
+        // Spring Security Custom Filter 적용 - Form '인증'에 대해서 적용
+
+
+        return http.build();
+    }
 }
