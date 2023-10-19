6fe8487d5d0ac02adf808e3e82e1467b5fa70d73
INUK-ai <arheon2469@gmail.com>
2023-10-19 21:22:39 +0900

    Refactor: Redis를 활용한 getMember, getGroup
    - 이게 성능 개선엔 큰 도움이 될 것 같지는 않아서 우선 주석 처리

--- wekiki/src/main/java/com/kakao/techcampus/wekiki/group/GroupService.java
+++ wekiki/src/main/java/com/kakao/techcampus/wekiki/group/GroupService.java
@@ -404,4 +404,39 @@ public class GroupService {
     protected Group getGroupById(Long groupId) {
         return groupJPARepository.findById(groupId).orElseThrow(() -> new Exception404("해당 그룹을 찾을 수 없습니다."));
     }
+
+    // RedisCache를 통한 getMember, getGroup -> 근데 성능 개선에 도움이 될까?
+    /*
+        protected Member getMemberById(Long memberId) {
+            try {
+                String memberKey = "member:" + memberId;
+                ValueOperations<String, Object> operations = redisTemplate.opsForValue();
+                if(redisTemplate.hasKey(memberKey)) {
+                    return (Member) operations.get(memberKey);
+                } else {
+                    Member member = memberJPARepository.findById(memberId).orElseThrow(() -> new Exception404("해당 사용자를 찾을 수 없습니다."));
+                    operations.set(memberKey, member);
+                    return member;
+                }
+            } catch (Exception e) {
+                throw new Exception500("서버 에러가 발생했습니다.");
+            }
+        }
+
+        protected Group getGroupById(Long groupId) {
+            try {
+                String groupKey = "group:" + groupId;
+                ValueOperations<String, Object> operations = redisTemplate.opsForValue();
+                if(redisTemplate.hasKey(groupKey)) {
+                    return (Group) operations.get(groupKey);
+                } else {
+                    Group group = groupJPARepository.findById(groupId).orElseThrow(() -> new Exception404("해당 그룹을 찾을 수 없습니다."));
+                    operations.set(groupKey, group);
+                    return group;
+                }
+            } catch (Exception e) {
+                throw new Exception500("서버 에러가 발생했습니다.");
+            }
+        }
+     */
 }
