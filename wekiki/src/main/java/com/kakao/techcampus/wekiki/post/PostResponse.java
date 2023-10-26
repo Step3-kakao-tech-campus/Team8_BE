b48f2e24831df48febb5493680012fd20e3a3bbe
kimdonwoo <ehstjstod@gmail.com>
2023-10-26 19:31:09 +0900

    Style : PostResponse 변수명 수정
    - history 관련해서 변수명 수정하였습니다.

--- wekiki/src/main/java/com/kakao/techcampus/wekiki/post/PostResponse.java
+++ wekiki/src/main/java/com/kakao/techcampus/wekiki/post/PostResponse.java
@@ -77,7 +77,7 @@ public class PostResponse {
             Long historyId;
             String title;
             String content;
-            LocalDateTime created_at;
+            LocalDateTime createdAt;
 
             public historyDTO(GroupMember groupMember,History history){
                 this.memberId = groupMember.getId();
@@ -85,7 +85,7 @@ public class PostResponse {
                 this.historyId = history.getId();
                 this.title = history.getTitle();
                 this.content = history.getContent();
-                this.created_at = history.getCreated_at();
+                this.createdAt = history.getCreated_at();
             }
 
         }
