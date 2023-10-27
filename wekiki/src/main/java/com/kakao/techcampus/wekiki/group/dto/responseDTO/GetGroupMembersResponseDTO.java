e46ed57a19b84d29d19cab1eb3b4bbab532ee282
INUK-ai <arheon2469@gmail.com>
2023-10-27 18:31:44 +0900

    Refactor: 그룹원 조회 기능 수정
    - 현재 활동중인 회원과 탈퇴한 회원 모두를 반환하는 것을 활동중인 회원만 반환하는 것으로 수정

--- wekiki/src/main/java/com/kakao/techcampus/wekiki/group/dto/responseDTO/GetGroupMembersResponseDTO.java
+++ wekiki/src/main/java/com/kakao/techcampus/wekiki/group/dto/responseDTO/GetGroupMembersResponseDTO.java
@@ -1,19 +1,23 @@
 package com.kakao.techcampus.wekiki.group.dto.responseDTO;
 
 import com.kakao.techcampus.wekiki.group.domain.Group;
-import com.kakao.techcampus.wekiki.group.domain.member.GroupMember;
+import com.kakao.techcampus.wekiki.group.domain.member.ActiveGroupMember;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 public class GetGroupMembersResponseDTO {
 
-    private List<GroupMember> groupMemberList;
+    private List<ActiveGroupMember> activeGroupMemberList;
 
     public GetGroupMembersResponseDTO(Group group) {
-        this.groupMemberList = group.getGroupMembers();
+        this.activeGroupMemberList = group.getGroupMembers().stream()
+                .filter(groupMember -> groupMember instanceof ActiveGroupMember)
+                .map(groupMember -> (ActiveGroupMember) groupMember)
+                .collect(Collectors.toList());
     }
 }
