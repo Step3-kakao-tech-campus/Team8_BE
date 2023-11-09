e457300a71e988b9d8a1ab13fe25f8fb4b7f8c75
INUK-ai <arheon2469@gmail.com>
2023-11-09 18:48:23 +0900

    Refactor: 로그 메세지 수정
    - 각 필요한 memberId, groupId 추가

--- wekiki/src/main/java/com/kakao/techcampus/wekiki/group/service/GroupService.java
+++ wekiki/src/main/java/com/kakao/techcampus/wekiki/group/service/GroupService.java
@@ -25,6 +25,7 @@ import com.kakao.techcampus.wekiki.post.PostJPARepository;
 import com.kakao.techcampus.wekiki.report.Report;
 import com.kakao.techcampus.wekiki.report.ReportJPARepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -36,6 +37,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
@@ -63,17 +65,21 @@ public class GroupService {
      */
     @Transactional
     public GroupResponseDTO.CreateUnOfficialGroupResponseDTO createUnOfficialGroup(GroupRequestDTO.CreateUnOfficialGroupRequestDTO requestDTO, Long memberId) {
+        
         // Group 생성
         Group group = switch (requestDTO.groupType()) {
             case UNOFFICIAL_CLOSED -> buildUnOfficialClosedGroup(requestDTO);
             case UNOFFICIAL_OPENED -> buildUnOfficialOpenedGroup(requestDTO);
             default -> throw new Exception400("유효하지 않은 그룹 유형입니다.");
         };
+        log.debug("그룹 생성 완료");
 
         // MemberId로부터 Member 찾기
         Member member = getMemberById(memberId);
         // GroupMember 생성
         GroupMember groupMember = buildGroupMember(member, group, requestDTO.groupNickName());
+        
+        log.debug("GroupMember 생성 완료");
 
         // Entity 저장
         group.addGroupMember(groupMember);
@@ -82,6 +88,8 @@ public class GroupService {
         groupJPARepository.save(group);
         memberJPARepository.save(member);
         groupMemberJPARepository.save(groupMember);
+        
+        log.debug("Entity 저장 완료");
 
         // return
         return new GroupResponseDTO.CreateUnOfficialGroupResponseDTO(group, invitationService.getGroupInvitationCode(group.getId()));
@@ -138,6 +146,8 @@ public class GroupService {
         Page<OfficialGroup> officialGroups = groupJPARepository.findOfficialGroupsByKeyword(keyword, pageable);
         // 비공식 공개 그룹 리스트
         Page<UnOfficialOpenedGroup> unOfficialOpenedGroups = groupJPARepository.findUnOfficialOpenedGroupsByKeyword(keyword, pageable);
+        
+        log.debug("그룹 리스트 조회 완료");
 
         return new GroupResponseDTO.SearchGroupDTO(officialGroups, unOfficialOpenedGroups);
     }
@@ -148,10 +158,13 @@ public class GroupService {
     public GroupResponseDTO.SearchOfficialGroupResponseDTO searchOfficialGroupByKeyword(String keyword, int page) {
         Pageable pageable = PageRequest.of(page, GROUP_SEARCH_SIZE);
 
-        // 비공식 공개 그룹 리스트
+        // 공식 그룹 리스트
         Page<OfficialGroup> officialGroups = groupJPARepository.findOfficialGroupsByKeyword(keyword, pageable);
 
+        log.debug("공식 그룹 리스트 조회 완료");
+
         if (officialGroups.isEmpty()) {
+            log.info("공식 그룹 마지막 페이지");
             throw new Exception404("마지막 페이지입니다.");
         }
 
@@ -167,7 +180,10 @@ public class GroupService {
         // 비공식 공개 그룹 리스트
         Page<UnOfficialOpenedGroup> unOfficialOpenedGroups = groupJPARepository.findUnOfficialOpenedGroupsByKeyword(keyword, pageable);
 
+        log.debug("공개 그룹 리스트 조회 완료");
+
         if (unOfficialOpenedGroups.isEmpty()) {
+            log.info("비공식 공개 그룹 마지막 페이지");
             throw new Exception404("마지막 페이지입니다.");
         }
 
@@ -179,6 +195,8 @@ public class GroupService {
      */
     public GroupResponseDTO.SearchGroupInfoDTO getGroupInfo(Long groupId) {
         Group group = getGroupById(groupId);
+        
+        log.debug("그룹 상세 정보 조회 완료");
 
         return new GroupResponseDTO.SearchGroupInfoDTO(group);
     }
@@ -192,12 +210,20 @@ public class GroupService {
         getMemberById(memberId);
 
         UnOfficialOpenedGroup group = groupJPARepository.findUnOfficialOpenedGroupById(groupId)
-                .orElseThrow(() -> new Exception404("그룹을 찾을 수 없습니다."));
+                .orElseThrow(() -> {
+                    log.info("그룹 조회 실패 " + memberId + " 번 회원, " + groupId + " 번 그룹");
+                    return new Exception404("그룹을 찾을 수 없습니다.");
+                });
+
+        log.debug("그룹 조회 완료");
 
         // 틀린 경우, 에러 핸들링
         if(!group.getEntrancePassword().equals(requestDTO.entrancePassword())) {
+            log.info("비밀번호 불일치 " + memberId + " 번 회원, " + groupId + " 번 그룹");
             throw new Exception400("비밀번호가 틀렸습니다.");
         }
+        
+        log.debug("비밀번호 인증 완료");
 
         redisUtils.setGroupIdValues(MEMBER_ID_PREFIX + memberId, groupId, Duration.ofHours(MEMBER_ID_LIFETIME));
     }
@@ -213,8 +239,9 @@ public class GroupService {
         Group group = getGroupById(groupId);
 
         checkJoinPermission(memberId, groupId);
+        
+        log.debug("가입 권한 확인 완료");
 
-        // 그룹 내 닉네임 중복 예외 처리
         String groupNickName = requestDTO.nickName();
 
         GroupMember wasGroupMember = groupMemberJPARepository.findGroupMemberByMemberIdAndGroupId(memberId, groupId);
@@ -222,10 +249,10 @@ public class GroupService {
         if(wasGroupMember != null) {
             // 이미 가입한 상태일 시 예외 처리
             if(wasGroupMember.isActiveStatus()) {
+                log.info("이미 가입된 회원 " + memberId + " 번 회원, " + groupId + " 번 그룹");
                 throw new Exception400("이미 가입된 회원입니다.");
             }
 
-            // 재가입 회원인지 확인
             wasGroupMember.changeStatus();
             wasGroupMember.update(requestDTO.nickName());
 
@@ -233,9 +260,13 @@ public class GroupService {
             saveGroupMember(member, group, wasGroupMember);
         } else {
             groupNickNameCheck(groupId, groupNickName);
+            
+            log.debug("그룹 닉네임 중복 확인 완료");
 
             saveGroupMember(member, group, buildGroupMember(member, group, groupNickName));
         }
+        
+        log.debug("그룹 참가 완료");
 
         redisUtils.deleteValues(MEMBER_ID_PREFIX + memberId);
     }
@@ -248,15 +279,21 @@ public class GroupService {
         groupJPARepository.save(group);
         memberJPARepository.save(member);
         groupMemberJPARepository.save(groupMember);
+        
+        log.debug("그룹 멤버 저장 완료");
     }
 
     protected void checkJoinPermission(Long memberId, Long groupId) {
         Object oGroupId = Optional.ofNullable(redisUtils.getValues(MEMBER_ID_PREFIX + memberId))
-                .orElseThrow(() -> new Exception404("해당 그룹에 가입할 수 없습니다."));
+                .orElseThrow(() -> {
+                    log.info("그룹 가입 권한 없음 " + memberId + " 번 회원, " + groupId + " 번 그룹");
+                    throw new Exception404("해당 그룹에 가입할 수 없습니다.");
+                });
 
         Long lGroupId = ((Integer) oGroupId).longValue();
 
         if(!lGroupId.equals(groupId)) {
+            log.info("현재 그룹에 대한 가입 권한 없음 " + memberId + " 번 회원, " + groupId + " 번 그룹");
             throw new Exception400("현재 그룹에 가입할 수 없습니다.");
         }
     }
@@ -267,6 +304,8 @@ public class GroupService {
     public GroupResponseDTO.GetGroupMembersResponseDTO getGroupMembers(Long groupId, Long memberId) {
         Group group = getGroupById(groupId);
         getActiveGroupMember(groupId, memberId);
+        
+        log.debug("그룹원 리스트 조회 완료");
 
         return new GroupResponseDTO.GetGroupMembersResponseDTO(group);
     }
@@ -281,6 +320,8 @@ public class GroupService {
         // 해당 멤버의 Post 기록 정보 확인(History에서 가져옴)
         Pageable pageable = PageRequest.of(0, 10);
         Page<History> myHistoryList = historyJPARepository.findAllByGroupMember(groupMember.getId(), pageable);
+        
+        log.debug("히스토리 조회 완료");
 
         // 그룹 이름, 현재 닉네임, Post 기록 정보를 담은 responseDTO 반환
         return new GroupResponseDTO.MyGroupInfoResponseDTO(groupMember.getGroup(), groupMember, myHistoryList);
@@ -295,6 +336,8 @@ public class GroupService {
 
         Pageable pageable = PageRequest.of(page, size);
         Page<History> myHistoryList = historyJPARepository.findAllByGroupMember(groupMember.getId(), pageable);
+
+        log.debug("히스토리 조회 완료");
 
         return new GroupResponseDTO.MyGroupHistoryResponseDTO(myHistoryList);
     }
@@ -313,11 +356,13 @@ public class GroupService {
 
         // 빈칸일 경우 예외 처리
         if(newNickName.isEmpty()) {
+            log.info("공백 닉네임 " + memberId + " 번 회원, " + groupId + " 번 그룹");
             throw new Exception400("공백은 닉네임이 될 수 없습니다.");
         }
 
         // 기존 닉네임과 같은 경우 예외 처리
         if(groupMember.getNickName().equals(newNickName)) {
+            log.info("기존과 동일한 닉네임 " + memberId + " 번 회원, " + groupId + " 번 그룹");
             throw new Exception400("기존 닉네임과 같은 닉네임입니다.");
         }
 
@@ -329,6 +374,8 @@ public class GroupService {
 
         // 저장
         groupMemberJPARepository.save(groupMember);
+
+        log.debug("그룹 닉네임 변경 완료");
     }
 
 
@@ -352,6 +399,8 @@ public class GroupService {
 
             groupJPARepository.save(group);
             groupMemberJPARepository.save(groupMember);
+            
+            log.debug("그룹 탈퇴 완료");
 
         } else {
             deleteGroup(group);
@@ -360,56 +409,89 @@ public class GroupService {
 
     private void deleteGroup(Group group) {
         List<GroupMember> groupMemberList = groupMemberJPARepository.findAllByGroupId(group.getId());
+        
+        log.debug("그룹 멤버 조회 완료");
 
         for (GroupMember groupMember : groupMemberList) {
             Long groupMemberId = groupMember.getId();
 
             List<Report> reportList = reportJPARepository.findAllByFromMemberId(groupMemberId);
             reportJPARepository.deleteAll(reportList);
+            
+            log.debug("리포트 삭제 완료");
 
             List<History> historyList = historyJPARepository.findAllByGroupMemberId(groupMemberId);
             historyJPARepository.deleteAll(historyList);
 
+            log.debug("히스토리 삭제 완료");
+
             List<Comment> commentList = commentJPARepository.findAllByGroupMemberId(groupMemberId);
             commentJPARepository.deleteAll(commentList);
+
+            log.debug("댓글 삭제 완료");
 
             List<Post> postList = postJPARepository.findAllByGroupMember(groupMemberId);
             postJPARepository.deleteAll(postList);
 
+            log.debug("포스트 삭제 완료");
+
             List<PageInfo> pageInfoList = pageJPARepository.findAllByGroupMember(group.getId());
             pageJPARepository.deleteAll(pageInfoList);
+
+            log.debug("페이지 삭제 완료");
 
             Member member = groupMember.getMember();
             member.getGroupMembers().remove(groupMember);
             memberJPARepository.save(member);
 
+            log.debug("회원 내 그룹 멤버 삭제 완료");
+
             group.removeGroupMember(groupMember);
             groupMemberJPARepository.delete(groupMember);
+
+            log.debug("그룹 멤버 삭제 완료");
         }
 
         groupJPARepository.delete(group);
+
+        log.debug("그룹 삭제 완료");
     }
 
     protected Member getMemberById(Long memberId) {
-        return memberJPARepository.findById(memberId).orElseThrow(() -> new Exception404("해당 사용자를 찾을 수 없습니다."));
+        return memberJPARepository.findById(memberId).orElseThrow(() -> {
+            log.info("사용자 조회 불가 " + memberId + " 번 회원");
+            throw new Exception404("해당 사용자를 찾을 수 없습니다.");
+        });
     }
 
     protected Group getGroupById(Long groupId) {
-        return groupJPARepository.findById(groupId).orElseThrow(() -> new Exception404("해당 그룹을 찾을 수 없습니다."));
+        return groupJPARepository.findById(groupId).orElseThrow(() -> {
+            log.info("그룹 조회 불가 " + groupId + " 번 그룹");
+            throw new Exception404("해당 그룹을 찾을 수 없습니다.");
+        });
     }
 
     private GroupMember getActiveGroupMember(Long groupId, Long memberId) {
         GroupMember groupMember = groupMemberJPARepository.findGroupMemberByMemberIdAndGroupId(memberId, groupId);
 
-        if (groupMember == null || !groupMember.isActiveStatus()) {
+        if (groupMember == null) {
+            log.info("권한 없는 그룹 접근 " + memberId + " 번 회원, " + groupId + " 번 그룹");
             throw new Exception404("해당 그룹에 속한 회원이 아닙니다.");
         }
+        
+        if(!groupMember.isActiveStatus()) {
+            log.info("탈퇴한 그룹 접근 " + memberId + " 번 회원, " + groupId + " 번 그룹");
+            throw new Exception404("해당 그룹에 속한 회원이 아닙니다.");
+        }
+        
+        log.debug("그룹 멤버 조회 완료");
 
         return groupMember;
     }
 
     protected void groupNickNameCheck(Long groupId, String groupNickName) {
         if(groupMemberJPARepository.findGroupMemberByNickName(groupId, groupNickName).isPresent()) {
+            log.info(groupId + " 번 그룹에서 사용 중인 닉네임");
             throw new Exception400("해당 닉네임은 이미 사용중입니다.");
         }
     }
